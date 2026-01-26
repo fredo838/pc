e30c8ad00ed2396b989e6a39a1396211c099cb16
Frederik Bode <fredo.bode@gmail.com>
2026-01-26 17:43:31 +0100

    improve

--- install.sh
+++ install.sh
@@ -1,14 +1,33 @@
+# Ubuntu installer
+# Keyboard "Belgian", variant "Belgian"
+
+# initial
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install git
-# bookmarks for firefox
-# 'saved passwords'
+sudo apt-get install git gedit
+
+# configure git + github
 git config --global user.email "fredo.bode@gmail.com"
 git config --global user.name "Frederik Bode"
 ssh-keygen -t ed25519 -C "fredo.bode@gmail.com" -f ~/.ssh/id_ed25519_personal
-
+ssh-add ~/.ssh/id_ed25519_personal
 # add this in github.com
 cat ~/.ssh/id_ed25519_personal.pub
 
-ssh-add ~/.ssh/id_ed25519_personal
 
+# VSCode:
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install code
+
+# Mozilla 
+# Login to account
+# Extensions:
+#   ClearURLs
+#   Privacy Badger
+#   uBlock Origin
+# about:config in address bar, then identity.fxaccounts.toolbar.pxiToolbarEnabled > Toggle (all OFF)
