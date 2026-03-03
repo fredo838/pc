a7ba9ea152e9fff8c7df98a0296cb3f05a83458f
Frederik Bode <frederik.bode@centrica.com>
2026-03-03 09:59:00 +0100

    update

--- install-keybindings.sh
+++ install-keybindings.sh
@@ -1,21 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-# Define the source path (change this to your backup file location)
+
 SOURCE_FILE="./vscode-keybindings.json"
 
-# Detect OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS path
     DEST_DIR="$HOME/Library/Application Support/Code/User"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux path
     DEST_DIR="$HOME/.config/Code/User"
 else
     echo "Unsupported OS: $OSTYPE"
     exit 1
 fi
 
-# Create directory if it doesn't exist (unlikely, but safe)
 mkdir -p "$DEST_DIR"
 
 # Perform the copy
