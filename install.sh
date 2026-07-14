8130113989daa4a49a4c0b99d6767a08f45892e1
Frederik Bode <fredo.bode@gmail.com>
2026-07-14 08:49:05 +0200

    update

--- install.sh
+++ install.sh
@@ -173,3 +173,9 @@ sudo apt-get install google-cloud-cli-docker-credential-gcr --yes
 #        }
 # }
 ## Install ghostty
+
+
+# Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update && sudo apt-get install -y google-chrome-stable
